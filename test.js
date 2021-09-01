071970f4d0eb0e14129ba4a9bdde187d4be039a1
DESKTOP-U11DQRD\ghost <dogiang146@gmail.com>
2021-09-01 14:29:31 +0700

    add commit

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 // test 2
 
 // Hoàn thành xong khóa test rebase
-// Test rb
+// Test rb lan 4
 // Test rebase lần 3 master
 // Hello how are you
 // Đã hoàn thành xong khóa học test rebase rồi ư !!!
